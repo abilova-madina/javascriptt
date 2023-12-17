e7271c9bbe726c35c19c55064d70a8ad8b5d5b6a
abilova-madina madi <abilovamadii@gmail.com>
2023-12-17 09:14:51 +0000

    demo.sass,app.js

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 "use strict";
 $(document).ready(function () {
 
+
+$('.btn').click(function (e) { 
+    e.preventDefault();
+    
+    let a = (a) => {
+        for (let i = 1; i <=a; i++) {
+            $(".col-md-12").append(`<h1> ${i}</h1`);
+        }
+    }
+    a($('input').val())
+});
+
+
+
 });

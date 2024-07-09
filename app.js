85349b7533d6c90f9bdc584bc64d0a48994753d7
abilova-madina madi <abilovamadii@gmail.com>
2024-07-09 19:19:52 +0000

    _demo.sass,app.js,index.html

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ $(document).ready(function () {
         e.preventDefault();
         if (!$.trim($('input').val()) == '') {
             let a = b => {
-                for (let i = 0; i < b; i++) { $('.textlist').append(`<h2>${b}</h2>`); }
+                for (let i = 0; i < b; i++) { $('.minibox').append(`<h2>${b}</h2>`); }
             }
-            let v = f => Number(f) ? a(f) : $('.textlist').append(`<h2>${f}</h2>`);
+            let v = f => Number(f) ? a(f) : $('.minibox').append(`<h2>${f}</h2>`);
             v($('input').val());
         }
         $('input').val('');
-    })
-
-
+    });
 
 });
